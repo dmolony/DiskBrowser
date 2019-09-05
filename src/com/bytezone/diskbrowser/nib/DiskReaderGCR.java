03a38e11f0497cf58a2a6fd262a798396aad1714
Denis Molony <dmolony@iinet.net.au>
2019-09-05 13:20:05 +1000

    tidying

--- src/com/bytezone/diskbrowser/nib/DiskReaderGCR.java
+++ src/com/bytezone/diskbrowser/nib/DiskReaderGCR.java
@@ -36,8 +36,8 @@ public class DiskReaderGCR extends DiskReader
       byte d1 = byteTranslator.decode (inBuffer[inPtr++]);
 
       // reassemble bytes
-      byte b0 = (byte) ((d0 & 0x3F) | ((d3 & 0x30) << 2));
-      byte b1 = (byte) ((d1 & 0x3F) | ((d3 & 0x0C) << 4));
+      byte b0 = (byte) (d0 | ((d3 & 0x30) << 2));
+      byte b1 = (byte) (d1 | ((d3 & 0x0C) << 4));
 
       // calculate running checksums
       outBuffer[outPtr++] = checksum (b0, checksums, 0, 2);
@@ -47,7 +47,7 @@ public class DiskReaderGCR extends DiskReader
         break;
 
       byte d2 = byteTranslator.decode (inBuffer[inPtr++]);      // translate
-      byte b2 = (byte) ((d2 & 0x3F) | ((d3 & 0x03) << 6));      // reassemble
+      byte b2 = (byte) (d2 | (d3 << 6));                        // reassemble
       outBuffer[outPtr++] = checksum (b2, checksums, 1, 0);     // checksum
     }
 
